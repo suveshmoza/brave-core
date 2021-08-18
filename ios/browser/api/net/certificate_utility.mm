48e720bf19edcb965686093a65b12bbee2b10e19
Brandon T <JustBrandonT@gmail.com>
2021-08-18 13:23:26 -0400

    Fixed formatting. Finished Certificate Utility to return all the SPKI from BraveCore for pinning.

---
+++ ios/browser/api/net/certificate_utility.mm
@@ -0,0 +1,143 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#import "brave/ios/browser/api/net/certificate_utility.h"
+
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "base/base64.h"
+#include "base/bind.h"
+#include "base/logging.h"
+#include "base/memory/ref_counted.h"
+#include "base/strings/sys_string_conversions.h"
+
+#include "net/cert/internal/cert_errors.h"
+#include "net/cert/internal/parsed_certificate.h"
+#include "net/cert/pem.h"
+#include "net/cert/x509_certificate.h"
+#include "net/cert/x509_util.h"
+#include "net/der/input.h"
+#include "net/http/hsts_info.h"
+#include "net/http/transport_security_state.h"
+#include "net/net_buildflags.h"
+#include "net/tools/transport_security_state_generator/cert_util.h"
+#include "net/tools/transport_security_state_generator/spki_hash.h"
+
+#include "third_party/boringssl/src/include/openssl/mem.h"
+#include "third_party/boringssl/src/include/openssl/sha.h"
+#include "third_party/boringssl/src/include/openssl/x509.h"
+
+namespace net {
+namespace {
+#if BUILDFLAG(INCLUDE_TRANSPORT_SECURITY_STATE_PRELOAD_LIST)
+#include "net/http/transport_security_state_static.h"  // nogncheck
+#endif  // INCLUDE_TRANSPORT_SECURITY_STATE_PRELOAD_LIST
+}
+}  // net
+
+namespace net {
+// from: net/tools/transport_security_state_generator/cert_util.h
+bool CalculateSPKIHashFromCertificate(X509* certificate,
+                                      std::uint8_t (&data)[32]) {
+  DCHECK(certificate);
+  bssl::UniquePtr<EVP_PKEY> key(X509_get_pubkey(certificate));
+  if (!key) {
+    return false;
+  }
+
+  uint8_t* spki_der;
+  size_t spki_der_len;
+  bssl::ScopedCBB cbb;
+  if (!CBB_init(cbb.get(), 0) ||
+      !EVP_marshal_public_key(cbb.get(), key.get()) ||
+      !CBB_finish(cbb.get(), &spki_der, &spki_der_len)) {
+    return false;
+  }
+
+  SHA256(spki_der, spki_der_len, data);
+  OPENSSL_free(spki_der);
+  return true;
+}
+}  // net
+
+@implementation BraveCertificateUtility
++ (NSArray<NSData*>*)acceptableCerts {
+  NSMutableArray* result = [[NSMutableArray alloc] init];
+  for (std::size_t i = 0; i < sizeof(net::kBraveAcceptableCerts) /
+                                  sizeof(net::kBraveAcceptableCerts[0]);
+       ++i) {
+    if (net::kBraveAcceptableCerts[i]) {
+      std::string data = std::string(net::kBraveAcceptableCerts[i]);
+      if (data.size() > 0) {
+        [result addObject:[NSData dataWithBytes:&data[0] length:data.size()]];
+      }
+    }
+  }
+  return result;
+}
+
++ (NSString*)pemEncodeCertificate:(SecCertificateRef)certificate {
+  base::ScopedCFTypeRef<CFDataRef> cert_data =
+      base::ScopedCFTypeRef<CFDataRef>(SecCertificateCopyData(certificate));
+  if (!cert_data) {
+    return nullptr;
+  }
+
+  bssl::UniquePtr<CRYPTO_BUFFER> cert_buffer(
+      net::X509Certificate::CreateCertBufferFromBytes(
+          reinterpret_cast<const char*>(CFDataGetBytePtr(cert_data)),
+          CFDataGetLength(cert_data)));
+
+  if (!cert_buffer) {
+    return nullptr;
+  }
+
+  std::string pem_encoded;
+  net::X509Certificate::GetPEMEncoded(cert_buffer.get(), &pem_encoded);
+  if (pem_encoded.empty()) {
+    return nullptr;
+  }
+
+  return base::SysUTF8ToNSString(pem_encoded);
+}
+
++ (NSData*)hashCertificateSPKI:(SecCertificateRef)certificate {
+  base::ScopedCFTypeRef<CFDataRef> cert_data =
+      base::ScopedCFTypeRef<CFDataRef>(SecCertificateCopyData(certificate));
+  if (!cert_data) {
+    return nullptr;
+  }
+
+  bssl::UniquePtr<CRYPTO_BUFFER> cert_buffer(
+      net::X509Certificate::CreateCertBufferFromBytes(
+          reinterpret_cast<const char*>(CFDataGetBytePtr(cert_data)),
+          CFDataGetLength(cert_data)));
+
+  if (!cert_buffer) {
+    return nullptr;
+  }
+
+  net::CertErrors errors;
+  scoped_refptr<net::ParsedCertificate> extended_cert =
+      scoped_refptr<net::ParsedCertificate>(net::ParsedCertificate::Create(
+          std::move(cert_buffer),
+          net::x509_util::DefaultParseCertificateOptions() /* {} */, &errors));
+
+  if (!extended_cert) {
+    LOG(ERROR) << errors.ToDebugString();
+    return nullptr;
+  }
+
+  std::uint8_t data[32] = {0};
+  net::der::Input spki = extended_cert->tbs().spki_tlv;
+  SHA256(spki.UnsafeData(), spki.Length(), data);
+
+  return [NSData dataWithBytes:data length:sizeof(data) / sizeof(data[0])];
+}
+@end
